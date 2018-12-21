06ec4a9bdc4e7f926edfac51d1090292626b2a61
luxiang0412 <1374920889@qq.com>
2018-12-21 10:23:01 +0800

    list深拷贝，浅拷贝

---
+++ src/main/java/luxiang/top/data_structures/lists/DeepcopyList.java
@@ -0,0 +1,18 @@
+package luxiang.top.data_structures.lists;
+
+import java.io.*;
+import java.util.List;
+
+public class DeepcopyList {
+    public static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+        out.writeObject(src);
+
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+        @SuppressWarnings("unchecked")
+        List<T> dest = (List<T>) in.readObject();
+        return dest;
+    }
+}
--- src/main/java/luxiang/top/data_structures/lists/Test.java
+++ src/main/java/luxiang/top/data_structures/lists/Test.java
@@ -1,12 +1,13 @@
 package luxiang.top.data_structures.lists;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -18,7 +19,7 @@ public class Test {
         //System.out.println(list);
         //[2, 3, 4]
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        /*ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0;i<30;i++){
             arr.add(i);
         }
@@ -46,6 +47,32 @@ public class Test {
         long time3 = System.currentTimeMillis();
         System.out.println();
         System.out.println("parallelStream用时（毫秒）："+(time3-time2)+"，list：");
-        System.out.println("stream用时（毫秒）："+(time2-time1)+"，list：");
+        System.out.println("stream用时（毫秒）："+(time2-time1)+"，list：");*/
+
+        //深度copy
+        ArrayList<ArrayList> parent = new ArrayList<ArrayList>();
+        ArrayList<Integer> child1 = new ArrayList<Integer>();
+        child1.add(1);
+        child1.add(2);
+        child1.add(3);
+        ArrayList<Integer> child2 = new ArrayList<Integer>();
+        child2.add(4);
+        child2.add(5);
+        child2.add(6);
+        ArrayList<Integer> child3 = new ArrayList<Integer>();
+        child3.add(7);
+        child3.add(8);
+        child3.add(9);
+        parent.add(child1);
+        parent.add(child2);
+        parent.add(child3);
+        //深拷贝
+        List<ArrayList> arrayLists = DeepcopyList.deepCopy(parent);
+        //浅拷贝
+        ArrayList arrayList = (ArrayList) parent.clone();
+        System.out.println(parent);
+        parent.get(1).remove(1);
+        System.out.println(arrayLists);
+        System.out.println(arrayList);
     }
 }
